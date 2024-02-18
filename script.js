a109bc884c9b3020b69bd3c517c8b98e1d74a941
Amber-Lea-P <amber.pontinen@gmail.com>
2024-02-18 10:58:11 +0200

    correction to script.js

--- script.js
+++ script.js
@@ -22,6 +22,16 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function goTown() {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+  }
+
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
@@ -29,6 +39,7 @@ function goStore() {
     button1.onclick = buyHealth;
     button2.onclick = buyWeapon;
     button3.onclick = goTown;
+    text.innerText = "You enter the store.";
   }
 
 function goCave() {
@@ -47,6 +58,3 @@ function goCave() {
   
   }
   
-  function goTown() {
-  
-  }

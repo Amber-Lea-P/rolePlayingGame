567e2c66e4d336862c5320073a6d80d1047526d2
Amber-Lea-P <amber.pontinen@gmail.com>
2024-02-24 13:30:24 +0200

    adding the fight location to the array

--- script.js
+++ script.js
@@ -47,7 +47,13 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
-      }
+      },
+      {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown], 
+        text: "You are fighting a monster."
+  }
   ];
 
 // initialize buttons
@@ -139,4 +145,11 @@ function goCave() {
   }
   function goFight() {
 
+  }
+
+  function attack() {
+
+  }
+  
+  function dodge() {
   }

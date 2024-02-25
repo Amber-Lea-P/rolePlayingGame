a12e1e6acebdf9bec4098b8407eec2275a8d2f3c
Amber-Lea-P <amber.pontinen@gmail.com>
2024-02-25 12:37:53 +0200

    adding additional locations and win  & lose functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let xp = 0;
 let health = 100;
-let gold = 999;
+let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
@@ -59,6 +59,18 @@ const locations = [
       "button text": ["Go to town square", "Go to town square", "Go to town square"],
       "button functions": [goTown, goTown, goTown],
       text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    },
+    {
+      name: "lose",
+      "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+      "button functions": [restart, restart, restart],
+      text: "You die. &#x2620;"
+    },
+    {
+      name: "win",
+      "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+      "button functions": [restart, restart, restart],
+      text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
     }
   ];
 
@@ -67,16 +79,17 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+//functions
 function update(location) {
+  monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
-  text.innerText = location.text;
+  text.innerHTML = location.text;
 }
-
 
 function goTown() {
     update(locations[0]);
@@ -90,7 +103,6 @@ function goCave() {
     update(locations[2]);
   }
 
-  
   function buyHealth() {
     if (gold >= 10) {
         gold -= 10;
@@ -168,7 +180,11 @@ function goCave() {
     if (health <= 0) {
       lose();
     } else if (monsterHealth <= 0) {
-      defeatMonster();
+      if (fighting === 2) { //if we are fighting then dragon, the last monster then we can win
+        winGame();
+      }else {
+        defeatMonster();
+      }
     }
   }
   
@@ -185,5 +201,22 @@ function goCave() {
   }
   
   function lose() {
-  
+    update(locations[5]);
   }
+
+  function restart() {
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ["stick"];
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+    goTown();
+  }
+
+  function winGame() {
+    update(locations[6]);
+  }
+  
